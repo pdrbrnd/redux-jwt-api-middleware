f98657589497f45e4c3db7b1c0d80bdebdcc2cea
Pedro Brandão <pedro@significa.pt>
2018-06-26 11:32:08 +0100

    Add router

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -1,3 +1,29 @@
 import React from "react"
+import { Switch, Route, Link } from "react-router-dom"
 
-export default () => <div>Hello world</div>
+export default () => (
+  <div>
+    <Switch>
+      <Route
+        exact
+        path="/"
+        render={() => (
+          <div>
+            <h1>Hello world from Home</h1>
+            <Link to="/about">About</Link>
+          </div>
+        )}
+      />
+      <Route
+        exact
+        path="/about"
+        render={() => (
+          <div>
+            <h1>Hello world from About</h1>
+            <Link to="/">Home</Link>
+          </div>
+        )}
+      />
+    </Switch>
+  </div>
+)
--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@
 import React from "react"
 import ReactDOM from "react-dom"
 import { Provider } from "react-redux"
+import { BrowserRouter } from "react-router-dom"
 
 import "./index.css"
 import App from "./components/App"
@@ -9,7 +10,9 @@ import configureStore from "./store/configureStore"
 
 ReactDOM.render(
   <Provider store={configureStore()}>
-    <App />
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
   </Provider>,
   document.getElementById("root")
 )
